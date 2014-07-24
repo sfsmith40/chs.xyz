661f6a4bb09da661529c34ad022b06044b958ede
Joah Gerstenberg <joahd@gbergfamily.com>
2014-07-23 19:05:37 -0500

    alright. here we go

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Chess::Application.routes.draw do
 
   get '/b/:id' => 'boards#show', :as => 'show_board'
 
+  # get '/websocket' => {:controller => 'websocket_rails', :action => ''}
+  # match '/websocket' => 'websocket_rails#'
+
+  match "/websocket", :to => WebsocketRails::ConnectionManager.new, via: [:get, :post]
+
+  get '/*path' => 'boards#index'
+
   root :to => 'boards#index'
 end
